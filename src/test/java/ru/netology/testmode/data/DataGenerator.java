f434fc1b65cf6e1fcbffdc317ccaae8c8cabe949
“Natali <natali-kontakt@yandex.ru>
2022-12-17 19:01:53 +0500

    Initial commit

---
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -0,0 +1,17 @@
+package ru.netology.testmode.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+import lombok.val;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+}
---
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -0,0 +1,12 @@
+package ru.netology.testmode.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+class AuthTest {
+
+
+}
